6ca27c26f4bcc73bab37c31852e79971e47491df
juancoficial7 <160972348+juancoficial7@users.noreply.github.com>
2024-09-23 00:16:20 -0500

    actividad 6 completada

---
+++ Friends/src/friends/Friends.java
@@ -0,0 +1,13 @@
+
+package friends;
+
+
+public class Friends {
+
+
+    public static void main(String[] args) {
+        MainForm frm = new MainForm();
+        frm.setVisible(true);
+    }
+    
+}
